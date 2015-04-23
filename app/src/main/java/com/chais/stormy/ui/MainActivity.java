0a8cf720d5fd5167435c07244f7a73a181ae0c69
enrique7mc <enrique7mc@gmail.com>
2015-04-22 21:25:22 -0500

    Add menu

--- app/src/main/java/com/chais/stormy/ui/MainActivity.java
+++ app/src/main/java/com/chais/stormy/ui/MainActivity.java
@@ -9,6 +9,8 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -83,6 +85,24 @@ public class MainActivity extends Activity implements
 		if (mGoogleApiClient.isConnected()) {
 			mGoogleApiClient.disconnect();
 		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 	private void getForecast() {
